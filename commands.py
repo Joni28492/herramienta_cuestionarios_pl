51818f4b185ed64f2f2daa3d98e01de333c3e161
Joni Fernandez <joni28492@gmail.com>
2025-10-19 20:35:09 +0200

    "menu de comandos en script python"

--- commands.py
+++ commands.py
@@ -9,28 +9,45 @@ astro_url = 'http://localhost:4321'
 init_path  = os.path.abspath('.')
 
 
-os.chdir(init_path)
-try:
-    subprocess.run(["git", "add", "."], shell=True)
-    subprocess.run(["git", "commit", "-m", '"prueba script python"'], shell=True)
-    subprocess.run(["git", "push"], shell=True)
 
-except subprocess.CalledProcessError as e:
-    print(e)
+def crear_commit():
+
+    msg = input("introduce msg commit: ")
+
+    os.chdir(init_path)
+    try:
+        subprocess.run(["git", "add", "."], shell=True)
+        subprocess.run(["git", "commit", "-m", f'"{msg}"'], shell=True)
+        subprocess.run(["git", "push"], shell=True)
+
+    except subprocess.CalledProcessError as e:
+        print("Error al ejecutar commit:", e)
+
+def ejecutar_astro_asturpol_game():
+    os.chdir(path_asturpol_game)
+
+    try:
+        subprocess.run(["npm", "run", "dev"], shell=True )
+    except subprocess.CalledProcessError as e:
+        print(f"Error al ejecutar npm run dev: {e}")
 
 
-# os.chdir(path_asturpol_game)
+def print_hola():
+    print("Hola")
 
-# try:
-#     subprocess.run(["npm", "run", "dev"], shell=True )
+if __name__ == '__main__':
+
+    commands = [ print_hola, ejecutar_astro_asturpol_game, crear_commit ]
+
+    for opt, c in enumerate(commands):
+        print(opt+1, c.__name__)
+    selected_otp = int(input("Que ejecutar: ")) - 1
+
+    print(f"Ejecutando commando: {commands[selected_otp].__name__}")
+    commands[selected_otp]()
     
-#     time.sleep(10)
-    
-    
-# except subprocess.CalledProcessError as e:
-#     print(f"Error al ejecutar npm run dev: {e}")
 
-
+    
 
 
 
