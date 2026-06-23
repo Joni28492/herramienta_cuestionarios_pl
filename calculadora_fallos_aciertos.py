234ffa6c69c4a305c10648b269d49c9f28491aa9
Joni Fernandez <joni28492@gmail.com>
2026-06-23 12:53:18 +0200

    "update 23 junio"

--- calculadora_fallos_aciertos.py
+++ calculadora_fallos_aciertos.py
@@ -1,1 +1,9 @@
-numero_preguntas = 
+numero_preguntas = int(input("Introduce num de preguntas: "))
+valor_pregunta = 100/numero_preguntas
+
+aciertos = int(input("Introduce num de aciertos: "))
+fallos = int(input("Introduce num de fallos: "))
+
+print(f"aciertos {aciertos} | fallos {fallos}")
+print(f"total sin restar: {aciertos*valor_pregunta}")
+print(f"total: { (aciertos*valor_pregunta)-((fallos*valor_pregunta)/3) }")
